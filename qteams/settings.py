d55df26402ecd48ce82b81836aab9737e446a2df
Joerg Sawatzki <sawatzki.joerg@gmail.com>
2020-05-01 18:25:35 +0200

    new static settings

--- qteams/settings.py
+++ qteams/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+STATIC_ROOT = '~/html/static'
 
 ALLOWED_HOSTS = [
     "api.qteams.team"
